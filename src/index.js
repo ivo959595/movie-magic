d7e8ab6308e98879d4d422f0fced887b00314843
ivo959595 <34497900+ivo959595@users.noreply.github.com>
2025-01-16 23:03:19 +0200

    install express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("working")
+import express from "express"
+
+
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send("Works")
+})
+
+app.listen(5000, () => console.log("Listening on port 5000"))
