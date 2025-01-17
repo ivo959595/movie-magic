72c695831fad260d248fc9611c2a4bcb903c7c52
ivo959595 <34497900+ivo959595@users.noreply.github.com>
2025-01-17 22:15:18 +0200

    add 404

--- src/index.js
+++ src/index.js
@@ -13,15 +13,18 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
-app.use('/static', express.static('src/public'))
+app.use("/static", express.static("src/public"));
 
 app.get("/", (req, res) => {
-  res.render('home')
+  res.render("home");
 });
 
-app.get("/about", (req,res) => {
-    res.render('about') 
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
+app.get("*", (req, res) => {
+  res.render("404");
+});
 
 app.listen(5000, () => console.log("Listening on port 5000"));
