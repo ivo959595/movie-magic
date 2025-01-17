e5d605670614283ca8a532bcd714abf32df60540
ivo959595 <34497900+ivo959595@users.noreply.github.com>
2025-01-17 21:54:53 +0200

    set up static

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
+app.use(express.static('src/public'))
+
 app.get("/", (req, res) => {
   res.render('home', {layout:false})
 });
