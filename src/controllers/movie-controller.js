abae7b57e93f62b5e673b8f922dc48ac074f47e1
ivo959595 <34497900+ivo959595@users.noreply.github.com>
2025-01-22 02:25:26 +0200

    add search page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -5,27 +5,23 @@ const movieController = Router();
 
 import movieService from "../services/movie-service.js";
 
+movieController.get("/search", (req, res) => {
+    res.render('search')
+})
+
 movieController.get("/create", (req, res) => {
   res.render("create");
 });
 
 movieController.post("/create", (req, res) => {
   const newMovie = req.body;
-
   movieService.create(newMovie)
-
-
   res.redirect('/');
 });
 
 movieController.get("/:movieId/details", (req, res) => {
   const movieId = req.params.movieId;
-
   const movie = movieService.findOne(movieId);
-
-  
-
-
   res.render("details", { movie });
 });
 
