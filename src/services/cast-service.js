2e95a24e269ba17d889d8833e62101473ef81fd1
ivo959595 <34497900+ivo959595@users.noreply.github.com>
2025-01-29 21:34:02 +0200

    show cast

--- src/services/cast-service.js
+++ src/services/cast-service.js
@@ -2,6 +2,11 @@ import Cast from "../models/Cast.js"
 
 
 export default {
+    getAll(){
+      return Cast.find({
+        
+      });  
+    },
     create(castData){
         return Cast.create(castData)
     }
