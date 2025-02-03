96c061f56c951022fa78408f0c25338b21d2d486
ivo959595 <34497900+ivo959595@users.noreply.github.com>
2025-02-03 23:54:42 +0200

    hesh passwprd

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -11,8 +11,9 @@ authController.get('/register', (req, res) => {
 authController.post('/register', async (req, res) => {
     const userData = req.body
 
-    await authService.register(userData);
+    
 
+    await authService.register(userData);
 
     res.end()
 })
--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,21 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     email: String,
     password: String,
+});
+
+userSchema.pre('save', async function (next) {
+    try {
+        if (this.isModified('password')) {
+            const salt = await bcrypt.genSalt(10); // Generate salt manually
+            this.password = await bcrypt.hash(this.password, salt);
+        }
+        next();
+    } catch (err) {
+        next(err); // Handle errors gracefully
+    }
 });
 
 const User = model('User', userSchema);
