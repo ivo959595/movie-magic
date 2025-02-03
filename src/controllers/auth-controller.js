b0eb616465c36ce83973b349c340df9cb2517243
ivo959595 <34497900+ivo959595@users.noreply.github.com>
2025-02-03 22:31:15 +0200

    update nav

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -3,6 +3,9 @@ import { Router } from "express";
 
 const authController = Router()
 
+authController.get('/register', (req, res) => {
+    res.render('auth/register')
+});
 
 export default authController; 
 
