bc77c081020e527b58ad9fd422c81441df1b7799
ivo959595 <34497900+ivo959595@users.noreply.github.com>
2025-02-03 22:36:59 +0200

    send registrer data

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -7,5 +7,12 @@ authController.get('/register', (req, res) => {
     res.render('auth/register')
 });
 
+authController.post('/register', async (req,res) => {
+    const userData = req.body
+    
+    res.end()
+})
+
+
 export default authController; 
 
